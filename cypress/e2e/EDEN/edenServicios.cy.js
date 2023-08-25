964a700cab7dfda39d3bb582f1e140f3beb2fc81
TamaraToledo <tamaramelisatoledo@gmail.com>
2023-08-25 14:55:20 -0300

    Test de servicios

--- cypress/e2e/EDEN/edenServicios.cy.js
+++ cypress/e2e/EDEN/edenServicios.cy.js
@@ -2,12 +2,21 @@
 
 describe("TEST DE SERVICIOS DE EDEN ENTRADAS", () => {
 
-    it("Verificar Servicio de INICIO", () => {
-     cy.request("GET","https://edenapi.edenentradas.com.ar/edenventarestapi2/api/contenido/inicio").then((respuesta)=>{
-      expect(respuesta.status).to.eq(200);
-     });
+   it("Verificar Servicio de INICIO", () => {
+    cy.request ("GET", "https://edenapi.edenentradas.com.ar/edenventarestapi/api/contenido/inicio"
+    ).then((respuesta)=>{
+    cy.log (`Respuesta de servicio de inicio: ${JSON.stringify(respuesta)}`);
+    expect(respuesta.status).to.eq(200);
+  });
   
+  it("Verificar Servicio de INICIO", () => {
+   cy.request ("GET", "https://edenapi.edenentradas.com.ar/edenventarestapi/api/contenido/inicio"
+   ).then((respuesta) => {
+   cy.log (`Respuesta de servicio de inicio: ${JSON.stringify(respuesta)}`);
+   expect(respuesta.status).to.eq(200);
+    });
      
     
     });
   })
+});
